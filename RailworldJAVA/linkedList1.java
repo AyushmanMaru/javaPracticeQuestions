202adfae3047fe66af514e097a2a0e3018ce7e61
AyushmanMaru <132875714+AyushmanMaru@users.noreply.github.com>
2024-05-07 17:12:15 +0530

    did some changes in code

--- RailworldJAVA/linkedList1.java
+++ RailworldJAVA/linkedList1.java
@@ -4,10 +4,15 @@ public class linkedList1 {
     public static void main(String[] args) {
         
        LinkedList<String> l = new LinkedList<String>();
+       LinkedList<String> l1 = new LinkedList<String>();
+       LinkedList<String> l2 = new LinkedList<String>();
 
        l.add("ayush");
        l.add("vivek");
        l.add("jai");
+
+       l1.add("Shiv");
+       l1.add("krish");
 
        System.out.println(l);
        l.addFirst("navin");
@@ -17,13 +22,25 @@ public class linkedList1 {
        System.out.println(l);
     //    l.remove();
     //    System.out.println(l);
-        l.remove(3);
-        System.out.println(l);
+        // l.remove(3);
+        // System.out.println(l);
 
         System.out.println(l.isEmpty());
+        System.out.println(l.size());
+        System.out.println(l.equals(l1));    //checking two collection object
 
-        l.clear();
-        System.out.println(l);
+        
+        l2.addAll(l);
+       System.out.println(l2);
+
+       System.out.println(l.equals(l2));
+       System.out.println(l.containsAll(l1));
+
+
+
+
+        // l.clear();
+        // System.out.println(l);
 
     }
 }
